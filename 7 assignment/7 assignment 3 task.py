40f56f2172a5591c26bb3589caf637f4fdb36d93
bulat <bulat2005k@mail.ru>
2025-11-06 18:37:52 +0500

    8 assignment

--- 7 assignment/7 assignment 3 task.py
+++ 7 assignment/7 assignment 3 task.py
@@ -26,14 +26,26 @@ while running:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_UP] and y - radius - step >= 0:
-        y -= step
+    if keys[pygame.K_UP] and y - radius >= 0:
+        if y - radius - step >= 0:
+            y -= step
+        else:
+            y -= y - radius
     if keys[pygame.K_DOWN] and y + radius + step <= HEIGHT:
-        y += step
-    if keys[pygame.K_LEFT] and x - radius - step >= 0:
-        x -= step
-    if keys[pygame.K_RIGHT] and x + radius + step <= WIDTH:
-        x += step
+        if y + radius + step <= HEIGHT:
+            y += step
+        else:
+            y += radius
+    if keys[pygame.K_LEFT] and x - radius >= 0:
+        if x - radius - step >= 0:
+            x -= step
+        else:
+            x -= x - radius
+    if keys[pygame.K_RIGHT] and x + radius <= WIDTH:
+        if x + radius + step <= WIDTH:
+            x += step
+        else:
+            x += radius
 
     screen.fill(WHITE)
     pygame.draw.circle(screen, RED, (x, y), radius)
